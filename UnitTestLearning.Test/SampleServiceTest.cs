ef625539be26f5eb4ec73ec2d67fc5c56a481be1
Hassan_Pezeshkiyan_1180 <hassanpezeshkiyan@gmail.com>
2023-07-12 08:56:10 +0330

    change on test class

--- UnitTestLearning.Test/SampleServiceTest.cs
+++ UnitTestLearning.Test/SampleServiceTest.cs
@@ -8,30 +8,38 @@ using System.Threading.Tasks;
 
 namespace UnitTestLearning.Test
 {
+    [TestClass]
     public class SampleServiceTest
     {
 
-        SampleService service = new SampleService();
-
+        [TestMethod]
         public void Sum_2_and_2_should_be_4()
         {
+            var service = new SampleService();
             var result = service.Sum(2, 2);
             result.Should().Be(4);
         }
+        
+        [TestMethod]
         public void Subtraction_4_and_2_should_be_2()
         {
+            var service = new SampleService();
             var result = service.Subtraction(4, 2);
             result.Should().Be(2);
         }
 
+        [TestMethod]
         public void Multiply_3_and_3_should_be_9()
         {
+            var service = new SampleService();
             var result = service.Multiply(3, 3);
             Assert.AreEqual(9, result);
         }
 
+        [TestMethod]
         public void Divide_4_and_2_should_be_2()
         {
+            var service = new SampleService();
             var result = service.Divide(4, 2);
             result.Should().Be(2);
         }
@@ -42,8 +50,10 @@ namespace UnitTestLearning.Test
         /// <remarks>   Hp, 7/12/2023. </remarks>
         ////////////////////////////////////////////////////////////////////////////////////////////////////
 
+        [TestMethod]
         public void Divide_4_and_0_should_be_0()
         {
+            var service = new SampleService();
             var result = service.Divide(4, 0);
             result.Should().Be(0);
         }
